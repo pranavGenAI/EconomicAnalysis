4aca7a096df8429a835e36b2b5fd28b77d0d527a
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-11-28 15:37:43 +0530

    app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ if "username" not in st.session_state:
 
 # Configure Google Generative AI with the API key
 #GOOGLE_API_KEY = st.secrets['GEMINI_API_KEY']
-GOOGLE_API_KEY = "AIzaSyCKXukz6eBYmFuA0kRX5boRPiaFOhfnUr0"
+GOOGLE_API_KEY = "AIzaSyBx5C6rkPdeo0Uy990h568DVawURcdRihQ"
 genai.configure(api_key=GOOGLE_API_KEY)
 
 def hash_password(password):
