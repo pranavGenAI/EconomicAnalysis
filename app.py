d670085ffdc35bd0405dc8562c5748f4817ad329
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-11-28 19:33:53 +0530

    app.py

--- app.py
+++ app.py
@@ -397,7 +397,11 @@ def main():
     st.markdown("")
     model = st.sidebar.selectbox(
         'Choose a model',
-        ['llama-3.1-70b-versatile', 'llama-3.1-70b-specdec', 'llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma-7b-it']
+        ['llama-3.1-70b-versatile', 'llama3-8b-8192', 'mixtral-8x7b-32768', 'gemma-7b-it']
+    )
+    st.sidebar.image(
+    "https://www.vgen.it/wp-content/uploads/2021/04/logo-accenture-ludo.png", 
+    use_column_width=True
     )
     conversational_memory_length = 5
     memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
